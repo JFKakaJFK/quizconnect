2593e598310fdf1192c7d2d20a5a80d40cf461eb
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-04-01 14:55:32 +0200

    basic creation deletion

--- src/main/java/at/qe/sepm/skeleton/services/QuestionService.java
+++ src/main/java/at/qe/sepm/skeleton/services/QuestionService.java
@@ -1,15 +1,17 @@
 package at.qe.sepm.skeleton.services;
 
 import at.qe.sepm.skeleton.model.Question;
+import at.qe.sepm.skeleton.model.QuestionSet;
 import at.qe.sepm.skeleton.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 /**
  * Service for accessing and manipulating {@link Question} entities.
  *
- * @author Lorenz_Smidt
+ * @author Johannes Koch
  *
  */
 @Component
@@ -19,8 +21,62 @@ public class QuestionService {
     @Autowired
     QuestionRepository questionRepository;
 
-    public Question saveQuestion(Question question){
-        return null;
+    /**
+     * Saves a {@link Question} to the db
+     *
+     * @param question
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public Question saveQuestion(Question question) throws IllegalArgumentException
+    {
+        // is the context valid?
+        if(question == null){
+            throw new IllegalArgumentException("Question cannot be null");
+        }
+        if(question.getQuestionSet() == null){
+            throw new IllegalArgumentException("Question must be associated to a QuestionSet");
+        }
+        if(question.getQuestionSet().isNew()){
+            throw new IllegalArgumentException("Save QuestionSet first");
+        }
+
+        // is the question itself valid?
+        if(question.getType() == null){
+            throw new IllegalArgumentException("Question Type can not be null");
+        }
+        if(question.getQuestionString() == null){
+            throw new IllegalArgumentException("Question can not be null");
+        }
+        if(question.getQuestionString().length() > 200){
+            throw new IllegalArgumentException("Question is too long(MAX: 200Chars)");
+        }
+
+        if(question.getRightAnswerString() == null){
+            throw new IllegalArgumentException("Answer can not be null");
+        }
+        if(question.getRightAnswerString().length() > 200){
+            throw new IllegalArgumentException("Answer is too long(MAX: 200Chars)");
+        }
+        // TODO more checks? how many wrong answers are mandatory?
+
+        return questionRepository.save(question);
+    }
+
+    /**
+     * Deletes a {@link Question} from the db
+     *
+     * @param question
+     */
+    @PreAuthorize("principal.username eq question.questionSet.author.user.username")
+    public void deleteQuestion(Question question){
+        // remove question from questionSet before deleting reference
+        /* TODO fix cyclic dependency moar abstraction?
+        QuestionSet questionSet = question.getQuestionSet();
+        questionSet.getQuestions().remove(question);
+        QuestionSet savedQuestionSet = ...
+        */
+        questionRepository.delete(question);
     }
 
 }
--- src/main/java/at/qe/sepm/skeleton/services/QuestionSetService.java
+++ src/main/java/at/qe/sepm/skeleton/services/QuestionSetService.java
@@ -1,17 +1,23 @@
 package at.qe.sepm.skeleton.services;
 
+import at.qe.sepm.skeleton.model.Manager;
 import at.qe.sepm.skeleton.model.Question;
 import at.qe.sepm.skeleton.model.QuestionSet;
 import at.qe.sepm.skeleton.repositories.QuestionRepository;
 import at.qe.sepm.skeleton.repositories.QuestionSetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Service for accessing and manipulating {@link Question} entities.
  *
- * @author Lorenz_Smidt
+ * @author Johannes Koch
  *
  */
 @Component
@@ -21,8 +27,70 @@ public class QuestionSetService {
     @Autowired
     QuestionSetRepository questionSetRepositoryRepository;
 
-    public Question saveQuestionSet(QuestionSet questionSet){
-        return null;
+    @Autowired
+    QuestionService questionService;
+
+    /**
+     * Finds all {@link QuestionSet}s where the name contains a search phrase
+     *
+     * @param name search phrase
+     * @return
+     */
+    public Collection<QuestionSet> getAllContaining(String name){
+        return questionSetRepositoryRepository.findByNameContaining(name);
+    }
+
+    /**
+     * Saves the {@link QuestionSet} to the db
+     *
+     * @param questionSet
+     * @return
+     * @throws IllegalArgumentException
+     */
+    @PreAuthorize("hasAuthority('MANAGER')")
+    public QuestionSet saveQuestionSet(QuestionSet questionSet) throws IllegalArgumentException{
+        if(questionSet == null){
+            throw new IllegalArgumentException("QuestionSet cannot be null!");
+        }
+        if(questionSet.getAuthor() == null){
+            throw new IllegalArgumentException("QuestionSet must have a manager");
+        }
+        if(questionSet.getName().length() > 100){
+            throw new IllegalArgumentException("QuestionSet name is too long(MAX: 100Chars)");
+        }
+        if(questionSet.getDescription().length() > 300){
+            throw new IllegalArgumentException("QuestionSet description is too long(MAX: 300Chars)");
+        }
+        if(questionSet.getDifficulty() == null){
+            throw new IllegalArgumentException("QuestionSet must have difficulty");
+        }
+
+        if(questionSet.isNew()){
+            Set<Question> questions = new HashSet<>(questionSet.getQuestions());
+            Set<Question> savedQuestions = new HashSet<>();
+            questionSet.setQuestions(null); // reset questions to avoid db inconsistency
+            QuestionSet savedQuestionSet = questionSetRepositoryRepository.save(questionSet);
+
+            for (Question q: questions) {
+                q.setQuestionSet(savedQuestionSet);
+                savedQuestions.add(questionService.saveQuestion(q));
+            }
+
+            savedQuestionSet.setQuestions(savedQuestions);
+            return questionSetRepositoryRepository.save(savedQuestionSet);
+        } else {
+            return questionSetRepositoryRepository.save(questionSet);
+        }
+    }
+
+    /**
+     * Deletes a {@link QuestionSet} from the db
+     *
+     * @param questionSet
+     */
+    @PreAuthorize("principal.username eq questionSet.author.user.username")
+    public void deleteQuestionSet(QuestionSet questionSet){
+        questionSetRepositoryRepository.delete(questionSet);
     }
 
 }
