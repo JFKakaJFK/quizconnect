125d3aa502773f6e2bde932d450fd44b3a0a895b
Johannes Spies <johannes.spies@student.uibk.ac.at>
2019-04-04 22:25:39 +0200

    CSV import and functionality

--- src/main/java/at/qe/sepm/skeleton/ui/beans/CSVImportBean.java
+++ src/main/java/at/qe/sepm/skeleton/ui/beans/CSVImportBean.java
@@ -1,60 +1,55 @@
 package at.qe.sepm.skeleton.ui.beans;
 
+import com.opencsv.CSVReader;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 import javax.annotation.ManagedBean;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ManagedBean
 public class CSVImportBean {
 
+
+    /**
+     * Handles fileupload
+     *
+     * @param event
+     */
     public void handleFileUpload(FileUploadEvent event){
-        System.out.println("test");
+        System.out.println("handleFileUpload");
         try {
             UploadedFile upload = event.getFile();
-            addUsersCSV(upload.getInputstream());
-        } catch (IOException e){
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_ERROR, "Error", "Die CSV Datei entspricht nicht dem verlangtem Format.")
-            );
+            addQuestionsFromCSV(upload.getInputstream());
+        } catch (IOException e) {
+            //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+            //        FacesMessage.SEVERITY_ERROR, "Error", "Die CSV Datei entspricht nicht dem verlangten Format.")
+            e.printStackTrace();
+            //);
         }
     }
 
-    private void addUsersCSV(InputStream inputStream){
-        try(BufferedReader buf = new BufferedReader(new InputStreamReader(inputStream))) {
-
-            String line;
-            try {
-                while((line = buf.readLine()) != null) {
-                    String[] userData = line.split(",");
-
-                    System.out.println("Wert 1:" + userData[0]);
-                    System.out.println("Wert 2:" + userData[1]);
-                    System.out.println("Wert 3:" + userData[2]);
-                    System.out.println("Wert 4:" + userData[3]);
-                    System.out.println("Wert 5:" + userData[4]);
-
-                }
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
-                        FacesMessage.SEVERITY_INFO, "Success", "Die Benutzer wurder erfolgreich hinzugefügt")
-                );
-            } catch (Exception e) {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
-                        FacesMessage.SEVERITY_ERROR, "Error", "Die CSV Datei entspricht nicht dem verlangtem Format.")
-                );
+    /**
+     * Uses a inputStream of a csv and converts to a String-Array
+     *
+     * @param inputStream
+     * @return List of List of String ([[question1][correctAnswer][wrongAnswer][wrongAnswer][wrongAnswer]][[question2]...]
+     */
+    private List<List<String>> addQuestionsFromCSV(InputStream inputStream) {
+        List<List<String>> records = new ArrayList<List<String>>();
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));) {
+            String[] values;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
             }
-
+            return records;
         } catch (IOException e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
-                    FacesMessage.SEVERITY_ERROR, "Error", "Fehler beim Benutzer hinzufügen")
-            );
+            e.printStackTrace();
         }
+        return records;
     }
 
 }
