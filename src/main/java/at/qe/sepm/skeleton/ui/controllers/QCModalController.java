62e0ca24a1e4c208d4b916322d05c68a2471ee98
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-04-08 01:26:39 +0200

    added basic modal

---
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/QCModalController.java
@@ -0,0 +1,9 @@
+package at.qe.sepm.skeleton.ui.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QCModalController {
+
+    public void defaultAction(){}
+}
