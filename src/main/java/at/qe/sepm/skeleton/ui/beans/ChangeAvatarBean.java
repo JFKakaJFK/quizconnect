ed841952e6235eedde8cde8709ebbcd8d6684919
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-04-07 22:47:27 +0200

    added player creation(very basic, will be turned into modal & moare validation)

--- src/main/java/at/qe/sepm/skeleton/ui/beans/ChangeAvatarBean.java
+++ src/main/java/at/qe/sepm/skeleton/ui/beans/ChangeAvatarBean.java
@@ -1,36 +1,35 @@
 package at.qe.sepm.skeleton.ui.beans;
 
-import at.qe.sepm.skeleton.model.Manager;
 import at.qe.sepm.skeleton.model.Player;
 import at.qe.sepm.skeleton.services.PlayerService;
 import at.qe.sepm.skeleton.services.StorageService;
-import org.hibernate.Hibernate;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.Serializable;
 
-//@Controller
-// @Scope("view")
-@Component
-@Transactional
+@Controller
+@Scope("view")
 public class ChangeAvatarBean implements Serializable {
 
     private static final Logger log = LoggerFactory.getLogger(ChangeAvatarBean.class);
 
-    @Autowired
     private StorageService storageService;
+    private PlayerService playerService;
 
     @Autowired
-    private PlayerService playerService;
+    public ChangeAvatarBean(StorageService storageService, PlayerService playerService){
+        assert storageService != null;
+        assert playerService != null;
+        this.storageService = storageService;
+        this.playerService = playerService;
+    }
 
     private String filename = null;
     private String status = "";
@@ -42,18 +41,14 @@ public class ChangeAvatarBean implements Serializable {
      *
      * @param event
      */
-    @Transactional
     public void handleAvatarUpload(FileUploadEvent event){
-        if(!Hibernate.isInitialized(player.getCreator())){
-            Hibernate.initialize(player.getCreator()); // throws exception
-        }
 
         if(filename != null){
             storageService.deleteAvatar(filename);
         }
         try {
             UploadedFile upload = event.getFile();
-            filename = storageService.storeAvatar(upload.getInputstream(), upload.getFileName(), player.getCreator().getId().toString());
+            filename = storageService.storeAvatar(upload.getInputstream(), upload.getFileName(), "1232"); // TODO player.getCreator().getId().toString());
             status = "Upload successful";
         } catch (IOException e){
             filename = null;
