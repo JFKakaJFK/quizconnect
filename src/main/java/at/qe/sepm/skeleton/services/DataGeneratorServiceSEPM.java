ff31e514ece23a2359920a01a4b113eab9cf57a7
Simon Triendl <csav3166@student.uibk.ac.at>
2019-05-05 21:02:09 +0200

    Added Questions and QuestionSets -- WIP

--- src/main/java/at/qe/sepm/skeleton/services/DataGeneratorServiceSEPM.java
+++ src/main/java/at/qe/sepm/skeleton/services/DataGeneratorServiceSEPM.java
@@ -7,17 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
-
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Set;
 
 /**
  * Service for generating test data
@@ -29,7 +25,7 @@ import java.util.Set;
 @Scope("application")
 public class DataGeneratorServiceSEPM {
 
-    public static final boolean enabled = true;
+    public static final boolean enabled = false;
 
     private final int countMultiplier = 1;
 
@@ -67,10 +63,69 @@ public class DataGeneratorServiceSEPM {
 
         generateManager(random.nextInt(5 * countMultiplier) + (10 * countMultiplier));
 
+        //generateQuestion(random.nextInt( 50 * countMultiplier) + (60 * countMultiplier));
+
+        //generateQuestionSet(random.nextInt(20 * countMultiplier) + (20 * countMultiplier));
+
+        generateQuestionSetAndQuestions(random.nextInt(20 * countMultiplier) + (10 *countMultiplier));
+
         AuthenticationUtil.clearAuthentication();
 
     }
 
+    /**
+     * WIP -- should create random String for Question and QuestionSet creation
+     * * @param maxLength
+     * @return
+     */
+
+    private String stringGenerator(int maxLength){
+        Random random = new Random();
+        maxLength =- 2;
+        ArrayList word = new ArrayList();
+        for (int i = 0; i <= random.nextInt(maxLength) + 1; i++){
+            word.add("x");
+        }
+        return word.toString();
+    }
+
+    /**
+     * Method that creates, saves to DB and returns a Question
+     * @param current
+     * @return
+     */
+    private QuestionSet getQuestionSet(int current){
+        Manager creator = managerService.getManagerById(101);
+
+        QuestionSet questionSet = new QuestionSet();
+        questionSet.setName("GeneratedSet" + current);
+        questionSet.setDescription("GeneratedDescription" + current);
+        questionSet.setDifficulty((current % 2 == 0) ? QuestionSetDifficulty.easy : QuestionSetDifficulty.hard);
+        questionSet.setAuthor(creator);
+
+        return questionSet;
+    }
+
+    /**
+     * Method that creates, saves to DB and return a QuestionSet
+     * @param set
+     * @param current
+     * @return
+     */
+    private Question getQuestion(int set ,int current){
+
+        Question question = new Question();
+        question.setType(QuestionType.text);
+        question.setQuestionString("GeneratedTestString" + set + " " + current);
+        question.setRightAnswerString("GeneratedRightAnswer" + set + " " + current);
+        question.setWrongAnswerString_1("GeneratedWrongAnswer1" + set + " " + current);
+        question.setWrongAnswerString_2("GeneratedWrongAnswer2" + set + " " + current);
+        question.setWrongAnswerString_3("GeneratedWrongAnswer3" + set + " " + current);
+        question.setWrongAnswerString_4("GeneratedWrongAnswer4" + set + " " + current);
+        question.setWrongAnswerString_5("GeneratedWrongAnswer5" + set + " " + current);
+
+        return question;
+    }
     /**
      * Method to generate a User
      * Formally used  at generatePlayer and generateManager
@@ -91,7 +146,10 @@ public class DataGeneratorServiceSEPM {
         return user;
     }
 
-
+    /**
+     * Method to generate Players (for test purposes only)
+     * @param count
+     */
     private void generatePlayer(int count){
         for (int i = 1; i <= count; i++){
 
@@ -106,8 +164,12 @@ public class DataGeneratorServiceSEPM {
         logger.info(count + " Player have been created");
     }
 
+    /**
+     * Method to generate Manager (for test purposes only)
+     * @param count
+     */
     private void generateManager(int count){
-        for (int i = 0; i <= count; i++){
+        for (int i = 1; i <= count; i++){
 
             Manager manager = new Manager();
             manager.setEmail("generatedMail@test.com" + i);
@@ -117,5 +179,59 @@ public class DataGeneratorServiceSEPM {
         }
 
         logger.info(count + " Manager have been created");
+    }
+
+    /**
+     * Method to generate Questions (for test purposes only)
+     * @param count
+     */
+    private void generateQuestion(int count){
+
+        QuestionSet questionSet = getQuestionSet(1);
+        questionSetService.saveQuestionSet(questionSet);
+
+        for (int i = 1; i <= count; i++){
+
+            Question question = getQuestion(1, i);
+            question.setQuestionSet(questionSet);
+            questionService.saveQuestion(question);
+        }
+
+        logger.info(count + " Questions have been created");
+    }
+
+    /**
+     * Method to generate QuestionSets (for test purposes only)
+     * @param count
+     */
+    private void generateQuestionSet(int count){
+
+        for (int i = 1; i <= count; i++){
+
+            QuestionSet questionSet = getQuestionSet(i);
+            questionSetService.saveQuestionSet(questionSet);
+        }
+
+        logger.info(count + " QuestionSets have been created");
+    }
+
+    /**
+     * Method to generate QuestionSets with each 30 associated Questions (for test purposes only)
+     * @param count
+     */
+    private void generateQuestionSetAndQuestions(int count){
+        for (int i = 1; i <= count; i++){
+
+            QuestionSet questionSet = getQuestionSet(i);
+            questionSetService.saveQuestionSet(questionSet);
+
+            for (int y = 1; y <= 30; y++){
+
+                Question question = getQuestion(i, y);
+                question.setQuestionSet(questionSet);
+                questionService.saveQuestion(question);
+            }
+        }
+        logger.info(count + "QuestionSet and " + count*30 + " Questions have been created");
     }
 }
