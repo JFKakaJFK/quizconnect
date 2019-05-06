cdf93b95e539f0869b2c3d886b2b2c0ed2183a9b
Andreas Oberhuber <csav4829@uibk.ac.at>
2019-05-06 18:22:30 +0200

    first selenium Tests

--- src/test/java/at/qe/sepm/skeleton/tests/selenium/seleniumTests.java
+++ src/test/java/at/qe/sepm/skeleton/tests/selenium/seleniumTests.java
@@ -1,19 +1,48 @@
 package at.qe.sepm.skeleton.tests.selenium;
 
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class seleniumTests {
 
-    @Test
-    public void firstTest() {
-        // Optional, if not specified, WebDriver will search your path for chromedriver.
-        System.setProperty("webdriver.chrome.driver", "C:\\Test\\chromedriver.exe");
+    private WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Test\\chromedriver.exe"); //
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void homePageTest() {
+
         driver.get("localhost:8080");
+        WebElement button = driver.findElement(By.xpath("//a[@class='btn btn-primary btn-lg']"));
+        button.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "http://localhost:8080/login.xhtml" );
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("http://localhost:8080/login.xhtml");
+        driver.findElement(By.id("username")).sendKeys("user1");
+        driver.findElement(By.id("password")).sendKeys("pw1");
+        WebElement button = driver.findElement(By.id("submit"));
+        button.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "http://localhost:8080/secured/home.xhtml" );
+    }
+
+    @After
+    public void shutDown() {
+        driver.close();
     }
 }
