b51d43bc94cba8c98a8956f7406358578ba14c64
Lorenz Smidt <lorenz.smidt@student.uibk.ac.at>
2019-05-09 17:11:31 +0200

    Added Player stat fields; TODO QuestionSetPerformance entity

--- src/main/java/at/qe/sepm/skeleton/model/Player.java
+++ src/main/java/at/qe/sepm/skeleton/model/Player.java
@@ -1,8 +1,9 @@
 package at.qe.sepm.skeleton.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -10,7 +11,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.springframework.data.domain.Persistable;
@@ -44,15 +44,152 @@ public class Player implements Persistable<Integer>
 	@ManyToOne(optional = false, fetch = FetchType.LAZY)
 	private Manager creator;
 	
+	private long stat_totalScore;
+	
+	private int stat_highScore;
+	
+	private int stat_correctAnswers;
+	
+	private int stat_totalAnswers;
+	
+	private long stat_playTime; // time in ms
+	
 	/**
-	 * List of player usernames the Player played with recently. Number of players limited by maxPlayedWithLast. Sorted from least recently (index 0) to most recently played with.
+	 * Map of {@link QuestionSet}s (represented by their ids) to the number of times the Player has played the QS. If no entry exists the QS has not been played yet. If a QS no longer exists, please use
+	 * the 'removeQSetFromPlayed()' function to remove it from the map.
+	 */
+	@ElementCollection(fetch = FetchType.EAGER)
+	private Map<Integer, Integer> qSetPlayCounts;
+	
+	/**
+	 * List of Players that were in the same game that this Player last played. Size is limited to maxPlayedWithLast but may be less. May be null.
 	 */
 	@ElementCollection(fetch = FetchType.EAGER)
 	private List<String> playedWithLast;
-
-	@Deprecated // TODO remove deprecated
-	@OneToMany(cascade = CascadeType.REMOVE, mappedBy = "player", fetch = FetchType.LAZY)
-	private List<QuestionSetPerformance> qSetPerformances;
+	
+	/**
+	 * Returns an iterator over the list of players the player recently played with. Iterator to prevent manipulation of reference.
+	 * 
+	 * @return
+	 */
+	public List<String> getPlayedWithLast()
+	{
+		return playedWithLast;
+	}
+	
+	/**
+	 * Stores the Players as last played with. Limits the number of stored players to maxPlayedWithLast.
+	 * 
+	 * @param players
+	 */
+	public void setPlayedWithLast(List<Player> players)
+	{
+		playedWithLast = new ArrayList<String>();
+		for (int i = 0; i < (players.size() > maxPlayedWithLast ? maxPlayedWithLast : players.size()); i++)
+		{
+			Player player = players.get(i);
+			if (player.getId() == this.id)
+				continue;
+			
+			playedWithLast.add(player.getUser().getUsername());
+		}
+	}
+	
+	public long getTotalScore()
+	{
+		return stat_totalScore;
+	}
+	
+	public void addToTotalScore(long additionalScore)
+	{
+		this.stat_totalScore += additionalScore;
+	}
+	
+	public int getHighScore()
+	{
+		return stat_highScore;
+	}
+	
+	public void setHighScore(int stat_highScore)
+	{
+		this.stat_highScore = stat_highScore;
+	}
+	
+	public int getCorrectAnswersCount()
+	{
+		return stat_correctAnswers;
+	}
+	
+	public void AddCorrectAnswers(int additionalRightAnswers)
+	{
+		this.stat_correctAnswers += additionalRightAnswers;
+	}
+	
+	public int getTotalAnswers()
+	{
+		return stat_totalAnswers;
+	}
+	
+	public void addTotalAnswers(int additionalTotalAnswers)
+	{
+		this.stat_totalAnswers += additionalTotalAnswers;
+	}
+	
+	public long getPlayTime()
+	{
+		return stat_playTime;
+	}
+	
+	public void addPlayTime(long additionalPlayTime)
+	{
+		this.stat_playTime += additionalPlayTime;
+	}
+	
+	public Map<Integer, Integer> getqSetPlayCounts()
+	{
+		return qSetPlayCounts;
+	}
+	
+	/**
+	 * Adds one play count to each QuestionSet in the list.
+	 * 
+	 * @param qSets
+	 */
+	public void addPlayToQSets(List<QuestionSet> qSets)
+	{
+		for (QuestionSet questionSet : qSets)
+		{
+			if (qSetPlayCounts.containsKey(questionSet.getId()))
+			{
+				qSetPlayCounts.put(questionSet.getId(), qSetPlayCounts.get(questionSet.getId()) + 1);
+			}
+			else
+			{
+				qSetPlayCounts.put(questionSet.getId(), 1);
+			}
+		}
+	}
+	
+	/**
+	 * Removes a QuestionSet id from the map of played ones. To be used if a QuestionSet no longer exists.
+	 * 
+	 * @param qSetId
+	 */
+	public void removeQSetFromPlayed(Integer qSetId)
+	{
+		if (qSetPlayCounts.containsKey(qSetId))
+		{
+			qSetPlayCounts.remove(qSetId);
+		}
+	}
+	
+	/**
+	 * Gets called automatically upon Player creation - DO NOT USE OTHERWISE!
+	 */
+	public void setqSetPlayCounts(Map<Integer, Integer> qSetPlayCounts)
+	{
+		this.qSetPlayCounts = qSetPlayCounts;
+	}
 	
 	public User getUser()
 	{
@@ -88,47 +225,6 @@ public class Player implements Persistable<Integer>
 	public void setCreator(Manager creator)
 	{
 		this.creator = creator;
-	}
-	
-	/**
-	 * Returns an iterator over the list of players the player recently played with. Iterator to prevent manipulation of reference.
-	 * 
-	 * @return
-	 */
-	public List<String> getPlayedWithLast()
-	{
-		return playedWithLast;
-	}
-	
-	/**
-	 * Adds the players' usernames to the list of players the player played with recently, removing duplicates and limits the number to maxPlayedWithLast.
-	 * 
-	 * @param player
-	 */
-	public void addToPlayedWithLast(Player player)
-	{
-		String username = player.getUser().getUsername();
-		if (playedWithLast.contains(username))
-		{
-			playedWithLast.remove(username); // remove from last position
-		}
-		else if (playedWithLast.size() > maxPlayedWithLast)
-		{
-			playedWithLast.remove(0); // remove least recently played with
-		}
-		
-		playedWithLast.add(username); // add new player at end of list (= most recently played with)
-	}
-	
-	public List<QuestionSetPerformance> getqSetPerformances()
-	{
-		return qSetPerformances;
-	}
-	
-	// TODO rewrite to add / update performances of individual QuestionSets
-	public void setqSetPerformances(List<QuestionSetPerformance> qSetPerformances)
-	{
-		this.qSetPerformances = qSetPerformances;
 	}
 	
 	public void setId(Integer id)
