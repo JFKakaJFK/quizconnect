b108a6d1f2186882613da41e96bcd36b95c729ce
Andreas Oberhuber <csav4829@uibk.ac.at>
2019-05-21 13:24:13 +0200

    fixed startup errors

---
+++ src/main/java/at/qe/sepm/skeleton/configs/Application.java
@@ -0,0 +1,22 @@
+package at.qe.sepm.skeleton.configs;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+
+@Configuration
+public class Application {
+
+    @Bean
+    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        return new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+            }
+        };
+    }
+}
