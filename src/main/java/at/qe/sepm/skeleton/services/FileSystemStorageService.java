d3eb5d8b40e3dce274452e20155963df40c8653f
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-06-10 12:36:27 +0200

    added delete all pics of set method

--- src/main/java/at/qe/sepm/skeleton/services/FileSystemStorageService.java
+++ src/main/java/at/qe/sepm/skeleton/services/FileSystemStorageService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -204,5 +205,15 @@ public class FileSystemStorageService implements StorageService {
         } catch (IOException e){
             log.error("Could not delete avatar " + answer);
         }
+    }
+
+    /**
+     * Deletes all answers of the {@link at.qe.sepm.skeleton.model.QuestionSet}.
+     * @param managerId
+     * @param questionSetId
+     */
+    @Override
+    public void deleteAllAnswersOfQuestionSet(String managerId, String questionSetId) {
+        FileSystemUtils.deleteRecursively(answers.resolve(managerId + "/" + questionSetId).toFile());
     }
 }
