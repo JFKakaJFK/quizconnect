758dd928e49946274a2cd2ac35583637ed9c0290
Andreas Oberhuber <csav4829@uibk.ac.at>
2019-06-24 20:31:34 +0200

    linux slashes

--- src/test/java/at/qe/sepm/skeleton/tests/homePageTest.java
+++ src/test/java/at/qe/sepm/skeleton/tests/homePageTest.java
@@ -20,7 +20,7 @@ public class homePageTest {
     private String testUsername = "user1";
     private String testPassword = "pw1";
     //private String driverPath = "src\\test\\java\\at\\qe\\sepm\\skeleton\\tests\\selenium\\chromedriver.exe";//chromedriver for 64-bit Version of Chrome 74
-    private String driverPath = "src\\test\\java\\at\\qe\\sepm\\skeleton\\tests\\selenium\\chromedriver";       //chromedriver for Linux 64-bit Version of Chrome 75
+    private String driverPath = "src/test/java/at/qe/sepm/skeleton/tests/selenium/chromedriver";       //chromedriver for Linux 64-bit Version of Chrome 75
 
     @Before
     public void setUp() {
